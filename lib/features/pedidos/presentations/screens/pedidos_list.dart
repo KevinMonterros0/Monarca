81aea0721ed31fbdc94eb29ee4cf7b5aa020bfae
Kevin Monterroso <proyectocrmmonarca@gmail.com>
2024-09-27 17:16:50 -0600

    Muesta la información del pedido

--- lib/features/pedidos/presentations/screens/pedidos_list.dart
+++ lib/features/pedidos/presentations/screens/pedidos_list.dart
@@ -1,10 +1,9 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:intl/intl.dart';
+import 'package:flutter/services.dart';
 import 'package:monarca/features/shared/infrastucture/services/key_value_storage_service_impl.dart';
 
 class OrdersListScreen extends ConsumerStatefulWidget {
@@ -53,67 +52,87 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
     }
   }
 
-  Future<void> changeOrderStatus(int idPedido, bool newStatus) async {
+  Future<void> showRouteDetails(int idPedido) async {
     try {
       final keyValueStorageService = KeyValueStorageServiceImpl();
       final token = await keyValueStorageService.getValue<String>('token');
 
-      final response = await http.put(
-        Uri.parse(
-            'https://apiproyectomonarca.fly.dev/api/pedidos/cambiarEstado/$idPedido'),
+      final response = await http.get(
+        Uri.parse('https://apiproyectomonarca.fly.dev/api/rutas/obtenerporPedido/$idPedido'),
         headers: {
           'Authorization': 'Bearer $token',
           'Content-Type': 'application/json',
         },
-        body: json.encode({
-          'estado': newStatus,
-        }),
       );
 
       if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Estado del pedido actualizado exitosamente')),
-        );
-        fetchOrders(); // Refrescar la lista de pedidos
+        final routeDetails = json.decode(response.body);
+        _showRouteDialog(context, routeDetails['direccion']);
       } else {
-        throw Exception('Error al cambiar el estado del pedido.');
+        throw Exception('Error al obtener la ruta del pedido.');
       }
     } catch (e) {
       print('Error: $e');
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content: Text('Ocurrió un error al cambiar el estado del pedido.')),
+        const SnackBar(content: Text('Ocurrió un error al obtener la ruta.')),
       );
     }
   }
 
-  DateTime getCurrentTimeInGuatemala() {
-    return DateTime.now().toUtc().subtract(const Duration(hours: 6));
+  void _showRouteDialog(BuildContext context, String direccion) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Detalles de la Ruta'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Dirección: $direccion'),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Clipboard.setData(ClipboardData(text: direccion));
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Dirección copiada al portapapeles.')),
+                  );
+                },
+                child: const Text('Copiar dirección'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cerrar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
-  Color getOrderCardColor(DateTime fechaEntrega, bool estadoPedido) {
-    final now = getCurrentTimeInGuatemala();
-    final durationToDelivery = fechaEntrega.difference(now);
+ Color getOrderCardColor(DateTime fechaEntrega, bool estadoPedido) {
+  final now = DateTime.now().toUtc().add(const Duration(hours: -6));
+  final durationToDelivery = fechaEntrega.difference(now);
 
-    if (!estadoPedido) {
-      return Colors.grey;
-    } else if (durationToDelivery.inHours >= 2) {
-      return Colors.green; 
-    } else if (durationToDelivery.inHours == 1) {
-      return Colors.yellow; 
-    } else if (durationToDelivery.inMinutes <= 60 &&
-        durationToDelivery.inMinutes > 0) {
-      return Colors.yellow;
-    } else if (now.isAfter(fechaEntrega) &&
-        now.difference(fechaEntrega).inMinutes <= 19) {
-      return Colors.yellow;
-    } else if (now.isAfter(fechaEntrega) &&
-        now.difference(fechaEntrega).inMinutes >= 20) {
-      return Colors.red;
-    } else {
-      return Colors.blue;
-    }
+  if (!estadoPedido) {
+    return Colors.grey;
+  } else if (durationToDelivery.inHours >= 2) {
+    return Colors.green; 
+  } else if (durationToDelivery.inHours == 1) {
+    return Colors.yellow;
+  } else if (durationToDelivery.inMinutes > 0 && durationToDelivery.inMinutes < 60) {
+    return Colors.yellow;
+  } else if (now.isAfter(fechaEntrega) && now.difference(fechaEntrega).inMinutes >= 20) {
+    return Colors.red;
+  } else {
+    return Colors.blue;
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -128,66 +147,30 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
               itemCount: allOrders.length,
               itemBuilder: (context, index) {
                 final order = allOrders[index];
-                final DateTime fechaEntrega =
-                    DateTime.parse(order['fecha_entrega']);
+                final DateTime fechaEntrega = DateTime.parse(order['fecha_entrega']);
                 final bool estadoPedido = order['estado_pedido'];
 
-                return Dismissible(
-                  key: Key(order['id_pedido'].toString()),
-                  background: Container(
-                    color: Colors.green,
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child:
-                        const Icon(Icons.check, color: Colors.white, size: 40),
+                return Card(
+                  color: getOrderCardColor(fechaEntrega, estadoPedido),
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  secondaryBackground: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerRight,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child:
-                        const Icon(Icons.cancel, color: Colors.white, size: 40),
-                  ),
-                  confirmDismiss: (direction) async {
-                    if (direction == DismissDirection.endToStart) {
-                      // Desactivar el pedido (siempre permitido)
-                      await changeOrderStatus(order['id_pedido'], false);
-                    } else if (direction == DismissDirection.startToEnd) {
-                      // Activar el pedido solo si la fecha actual no ha pasado de la fecha de entrega
-                      final nowInGuatemala = getCurrentTimeInGuatemala();
-                      if (nowInGuatemala.isBefore(fechaEntrega)) {
-                        await changeOrderStatus(order['id_pedido'], true);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text(
-                                  'No se puede activar un pedido cuya fecha de entrega ya ha pasado.')),
-                        );
-                      }
-                    }
-                    return false; // No eliminar automáticamente
-                  },
-                  child: Card(
-                    color: getOrderCardColor(fechaEntrega, estadoPedido),
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  child: ListTile(
+                    title: Text('Pedido #${order['id_pedido']}'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Cliente: ${order['cliente']}'),
+                        Text('Empleado: ${order['empleado']}'),
+                        Text('Fecha de Entrega: ${DateFormat('yyyy-MM-dd HH:mm').format(fechaEntrega)}'),
+                        Text('Total: Q${order['totalpedido']}'),
+                        Text('Estado del Pedido: ${estadoPedido ? 'Activo' : 'Inactivo'}'),
+                      ],
                     ),
-                    child: ListTile(
-                      title: Text('Pedido #${order['id_pedido']}'),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Cliente: ${order['cliente']}'),
-                          Text('Empleado: ${order['empleado']}'),
-                          Text(
-                              'Fecha de Entrega: ${DateFormat('yyyy-MM-dd HH:mm').format(fechaEntrega)}'),
-                          Text('Total: Q${order['totalpedido']}'),
-                          Text(
-                              'Estado del Pedido: ${estadoPedido ? 'Activo' : 'Inactivo'}'),
-                        ],
-                      ),
-                    ),
+                    onTap: () {
+                      showRouteDetails(order['id_pedido']);
+                    },
                   ),
                 );
               },

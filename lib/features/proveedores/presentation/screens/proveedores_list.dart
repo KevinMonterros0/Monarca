593d7f7f81b54c5f1cfafda58a43f4654363ea9a
Kevin <proyectocrmmonarca@gmail.com>
2024-10-18 00:19:13 -0600

    Modificación de provvedores lista

--- lib/features/proveedores/presentation/screens/proveedores_list.dart
+++ lib/features/proveedores/presentation/screens/proveedores_list.dart
@@ -27,7 +27,8 @@ class ProductListScreen extends StatefulWidget {
 class _ProductListScreenState extends State<ProductListScreen> {
   List<int> quantities = [];
   double productTotal = 0.0;
-  final TextEditingController _invoiceNumberController = TextEditingController();
+  final TextEditingController _invoiceNumberController =
+      TextEditingController();
   String? _invoiceError;
 
   @override
@@ -38,28 +39,30 @@ class _ProductListScreenState extends State<ProductListScreen> {
 
   Future<bool> _showExitConfirmationDialog(BuildContext context) async {
     return await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Confirmar salida'),
-        content: const Text('¿Deseas salir? Todos los valores totales serán restablecidos a 0 y el carrito será limpiado.'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(false),
-            child: const Text('No'),
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Confirmar salida'),
+            content: const Text(
+                '¿Deseas salir? Todos los valores totales serán restablecidos a 0 y el carrito será limpiado.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('No'),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    globalTotalAmount = 0.0;
+                    cart.clear();
+                  });
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Sí'),
+              ),
+            ],
           ),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                globalTotalAmount = 0.0;
-                cart.clear();
-              });
-              Navigator.of(context).pop(true); 
-            },
-            child: const Text('Sí'),
-          ),
-        ],
-      ),
-    ) ?? false;
+        ) ??
+        false;
   }
 
   Future<bool> _onWillPop() async {
@@ -77,25 +80,25 @@ class _ProductListScreenState extends State<ProductListScreen> {
     return null;
   }
 
-  void _updateCart(dynamic product, int quantity) {
-    final existingProductIndex = cart.indexWhere((item) => item['id'] == product['id_producto']);
+  void _updateCart(dynamic product, int internalQuantity, double productPrice) {
+  final existingProductIndex = cart.indexWhere((item) => item['id'] == product['id_producto']);
 
-    if (existingProductIndex != -1) {
-      if (quantity == 0) {
-        cart.removeAt(existingProductIndex);
-      } else {
-        cart[existingProductIndex]['quantity'] = quantity;
-      }
-    } else if (quantity > 0) {
-      cart.add({
-        'id': product['id_producto'],
-        'nombre': product['nombre'],
-        'precio': product['precio_compra'],
-        'quantity': quantity,
-        'id_proveedor': widget.supplierId,
-      });
+  if (existingProductIndex != -1) {
+    if (internalQuantity <= 0) {
+      cart.removeAt(existingProductIndex);
+    } else {
+      cart[existingProductIndex]['quantity'] = internalQuantity;
     }
+  } else if (internalQuantity > 0) {
+    cart.add({
+      'id': product['id_producto'],
+      'nombre': product['nombre'] == 'Agua' ? 'Pipa de agua' : product['nombre'],
+      'precio': productPrice,
+      'quantity': internalQuantity,
+      'id_proveedor': widget.supplierId,
+    });
   }
+}
 
   void _removeFromCart(Map<String, dynamic> product, int index) {
     setState(() {
@@ -105,10 +108,11 @@ class _ProductListScreenState extends State<ProductListScreen> {
   }
 
   void _sendPurchaseRequest(BuildContext context, String noFactura) async {
-    final localContext = context; 
+    final localContext = context;
 
     try {
-      final token = await KeyValueStorageServiceImpl().getValue<String>('token');
+      final token =
+          await KeyValueStorageServiceImpl().getValue<String>('token');
       final purchaseResponse = await http.post(
         Uri.parse('https://apiproyectomonarca.fly.dev/api/compras/crear'),
         headers: {
@@ -121,16 +125,20 @@ class _ProductListScreenState extends State<ProductListScreen> {
           'Id_proveedor': cart.first['id_proveedor'],
         }),
       );
+      print(purchaseResponse.body);
+      print(purchaseResponse.statusCode);
 
       if (purchaseResponse.statusCode == 201) {
         final purchaseData = json.decode(purchaseResponse.body);
         final int idCompra = purchaseData['id_compra'];
 
         for (var product in cart) {
-          double productTotal = (product['precio'] as num).toDouble() * product['quantity'].toDouble();
+          double productTotal = (product['precio'] as num).toDouble() *
+              product['quantity'].toDouble();
 
           final detailResponse = await http.post(
-            Uri.parse('https://apiproyectomonarca.fly.dev/api/detalleCompras/crear'),
+            Uri.parse(
+                'https://apiproyectomonarca.fly.dev/api/detalleCompras/crear'),
             headers: {
               'Authorization': 'Bearer $token',
               'Content-Type': 'application/json',
@@ -144,7 +152,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
           );
 
           if (detailResponse.statusCode != 201) {
-            throw Exception('Error al enviar el detalle del producto ${product['nombre']}.');
+            throw Exception(
+                'Error al enviar el detalle del producto ${product['nombre']}.');
           }
         }
 
@@ -168,7 +177,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
       if (mounted) {
         Navigator.of(context).pop();
         ScaffoldMessenger.of(localContext).showSnackBar(
-          const SnackBar(content: Text('Ocurrió un error al realizar la compra.')),
+          const SnackBar(
+              content: Text('Ocurrió un error al realizar la compra.')),
         );
       }
     }
@@ -193,65 +203,91 @@ class _ProductListScreenState extends State<ProductListScreen> {
         body: Column(
           children: [
             Expanded(
-              child: widget.products.isEmpty
-                  ? const Center(child: Text('No hay productos para este proveedor.'))
-                  : ListView.builder(
-                      padding: const EdgeInsets.all(10),
-                      itemCount: widget.products.length,
-                      itemBuilder: (context, index) {
-                        final product = widget.products[index];
+                child: widget.products.isEmpty
+                    ? const Center(
+                        child: Text('No hay productos para este proveedor.'))
+                    : ListView.builder(
+                        padding: const EdgeInsets.all(10),
+                        itemCount: widget.products.length,
+                        itemBuilder: (context, index) {
+                          final product = widget.products[index];
 
-                        return Card(
-                          elevation: 2,
-                          margin: const EdgeInsets.symmetric(vertical: 10),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: ListTile(
-                            leading: const Icon(Icons.shopping_cart, size: 40),
-                            title: Text(
-                              product['nombre'],
-                              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          // Verificar si el nombre del producto es 'Agua'
+                          final productName = product['nombre'] == 'Agua'
+                              ? 'Pipa de agua'
+                              : product['nombre'];
+                          final productPrice = product['nombre'] == 'Agua'
+                              ? product['precio_compra'] * 10000
+                              : product['precio_compra'];
+
+                          return Card(
+                            elevation: 2,
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                            subtitle: Text('Precio: Q${product['precio_compra']}'),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                  icon: const Icon(Icons.remove),
-                                  onPressed: () {
-                                    if (quantities[index] > 0) {
+                            child: ListTile(
+                              leading:
+                                  const Icon(Icons.shopping_cart, size: 40),
+                              title: Text(
+                                productName,
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text('Precio: Q$productPrice'),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.remove),
+                                    onPressed: () {
+                                      if (quantities[index] > 0) {
+                                        setState(() {
+                                          quantities[index]--;
+
+                                          final internalQuantity =
+                                              product['nombre'] == 'Agua'
+                                                  ? quantities[index] * 10000
+                                                  : quantities[index];
+
+                                          productTotal -= productPrice;
+                                          globalTotalAmount -= productPrice;
+                                          _updateCart(product, internalQuantity,
+                                              productPrice);
+                                        });
+                                      }
+                                    },
+                                  ),
+                                  Text('${quantities[index]}',
+                                      style: const TextStyle(fontSize: 18)),
+                                  IconButton(
+                                    icon: const Icon(Icons.add),
+                                    onPressed: () {
                                       setState(() {
-                                        quantities[index]--;
-                                        productTotal -= product['precio_compra'].toDouble();
-                                        globalTotalAmount -= product['precio_compra'].toDouble();
-                                        _updateCart(product, quantities[index]);
+                                        quantities[index]++;
+
+                                        final internalQuantity =
+                                            product['nombre'] == 'Agua'
+                                                ? quantities[index] * 10000
+                                                : quantities[index];
+
+                                        productTotal += productPrice;
+                                        globalTotalAmount += productPrice;
+                                        _updateCart(product, internalQuantity,
+                                            productPrice);
                                       });
-                                    }
-                                  },
-                                ),
-                                Text('${quantities[index]}', style: const TextStyle(fontSize: 18)),
-                                IconButton(
-                                  icon: const Icon(Icons.add),
-                                  onPressed: () {
-                                    setState(() {
-                                      quantities[index]++;
-                                      productTotal += product['precio_compra'].toDouble();
-                                      globalTotalAmount += product['precio_compra'].toDouble();
-                                      _updateCart(product, quantities[index]);
-                                    });
-                                  },
-                                ),
-                              ],
+                                    },
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-            ),
+                          );
+                        },
+                      )),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text('Total: Q$globalTotalAmount', style: const TextStyle(fontSize: 20)),
+              child: Text('Total: Q$globalTotalAmount',
+                  style: const TextStyle(fontSize: 20)),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -269,7 +305,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
                 ElevatedButton(
                   onPressed: () {
                     ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Producto agregado exitosamente.')),
+                      const SnackBar(
+                          content: Text('Producto agregado exitosamente.')),
                     );
                   },
                   child: const Text('Aceptar'),
@@ -297,7 +334,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
                       padding: EdgeInsets.all(8.0),
                       child: Text(
                         'Resumen de Productos Comprados',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
                       ),
                     ),
                     TextField(
@@ -305,7 +343,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
                       decoration: InputDecoration(
                         labelText: 'Número de Factura',
                         errorText: _invoiceError,
-                        contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
@@ -320,7 +359,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
                       },
                     ),
                     cart.isEmpty
-                        ? const Center(child: Text('No hay productos en el carrito.'))
+                        ? const Center(
+                            child: Text('No hay productos en el carrito.'))
                         : ListView.builder(
                             shrinkWrap: true,
                             padding: const EdgeInsets.all(10),
@@ -329,7 +369,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
                               final product = cart[index];
                               return ListTile(
                                 title: Text(product['nombre']),
-                                subtitle: Text('Cantidad: ${product['quantity']} | Precio: Q${product['precio']}'),
+                                subtitle: Text(
+                                    'Cantidad: ${product['quantity']} | Precio: Q${product['precio']}'),
                                 trailing: IconButton(
                                   icon: const Icon(Icons.delete),
                                   onPressed: () {
@@ -343,7 +384,8 @@ class _ProductListScreenState extends State<ProductListScreen> {
                           ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text('Total: Q$globalTotalAmount', style: const TextStyle(fontSize: 20)),
+                      child: Text('Total: Q$globalTotalAmount',
+                          style: const TextStyle(fontSize: 20)),
                     ),
                     Visibility(
                       visible: cart.isNotEmpty,

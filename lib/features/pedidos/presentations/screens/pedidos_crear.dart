cfde9eb72ecec25bac2d0e4f5a5303c1246948ea
Kevin <proyectocrmmonarca@gmail.com>
2024-10-18 16:33:14 -0600

    Modificación de scrollbar

--- lib/features/pedidos/presentations/screens/pedidos_crear.dart
+++ lib/features/pedidos/presentations/screens/pedidos_crear.dart
@@ -487,101 +487,109 @@ class _OrdersScreenState extends ConsumerState<OrdersScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: _showExitWarning,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Productos'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.shopping_cart),
-              onPressed: _showCart,
-            ),
-          ],
-        ),
-        body: isLoading
-            ? const Center(child: CircularProgressIndicator())
-            : SingleChildScrollView(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.all(10),
-                  itemCount: allProducts.length,
-                  itemBuilder: (context, index) {
-                    final product = allProducts[index];
-
-                    return Card(
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/${product['imagen']}',
-                            fit: BoxFit.cover,
-                            width: 150,
-                            height: 150,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              product['nombre'],
-                              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              'Q${product['precio']}',
-                              style: const TextStyle(fontSize: 14),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              IconButton(
-                                icon: const Icon(Icons.remove),
-                                onPressed: () {
-                                  if (quantities[index] > 0) {
-                                    setState(() {
-                                      quantities[index]--;
-                                      globalTotalAmount -= product['precio'];
-                                      _updateCart(product, quantities[index]);
-                                    });
-                                  }
-                                },
-                              ),
-                              Text('${quantities[index]}', style: const TextStyle(fontSize: 18)),
-                              IconButton(
-                                icon: const Icon(Icons.add),
-                                onPressed: () {
-                                  if (normalizeText(product['nombre']) == 'garrafon') {
-                                    _askIfGarrafonIsNew(context, product, index);
-                                  } else if (normalizeText(product['nombre']) == 'fardo de botellas') {
-                                    agregarFardoDeBotellas(product, index);
-                                  } else if (normalizeText(product['nombre']) == '25 bolsas') {
-                                    agregarBolsasDeAgua(product, index);
-                                  } else {
-                                    setState(() {
-                                      quantities[index]++;
-                                      globalTotalAmount += product['precio'];
-                                      _updateCart(product, quantities[index]);
-                                    });
-                                  }
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
+  return WillPopScope(
+    onWillPop: _showExitWarning,
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text('Productos'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: _showCart,
+          ),
+        ],
       ),
-    );
-  }
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Scrollbar(
+              thumbVisibility: true, 
+              child: ListView.builder(
+                padding: const EdgeInsets.all(10),
+                itemCount: allProducts.length,
+                itemBuilder: (context, index) {
+                  final product = allProducts[index];
+
+                  return Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/${product['imagen']}',
+                          fit: BoxFit.cover,
+                          width: 150,
+                          height: 150,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            product['nombre'],
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            'Q${product['precio']}',
+                            style: const TextStyle(fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.remove),
+                              onPressed: () {
+                                if (quantities[index] > 0) {
+                                  setState(() {
+                                    quantities[index]--;
+                                    globalTotalAmount -= product['precio'];
+                                    _updateCart(product, quantities[index]);
+                                  });
+                                }
+                              },
+                            ),
+                            Text('${quantities[index]}',
+                                style: const TextStyle(fontSize: 18)),
+                            IconButton(
+                              icon: const Icon(Icons.add),
+                              onPressed: () {
+                                if (normalizeText(product['nombre']) ==
+                                    'garrafon') {
+                                  _askIfGarrafonIsNew(context, product, index);
+                                } else if (normalizeText(
+                                        product['nombre']) ==
+                                    'fardo de botellas') {
+                                  agregarFardoDeBotellas(product, index);
+                                } else if (normalizeText(
+                                        product['nombre']) ==
+                                    '25 bolsas') {
+                                  agregarBolsasDeAgua(product, index);
+                                } else {
+                                  setState(() {
+                                    quantities[index]++;
+                                    globalTotalAmount += product['precio'];
+                                    _updateCart(product, quantities[index]);
+                                  });
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+    ),
+  );
+}
+
 }

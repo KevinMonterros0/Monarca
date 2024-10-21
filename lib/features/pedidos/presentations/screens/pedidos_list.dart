2ed0311a68f5c3d2e95ced90deed27d46b37298c
Kevin <proyectocrmmonarca@gmail.com>
2024-10-20 23:54:05 -0600

    Filtro de fechas

--- lib/features/pedidos/presentations/screens/pedidos_list.dart
+++ lib/features/pedidos/presentations/screens/pedidos_list.dart
@@ -17,13 +17,46 @@ class OrdersListScreen extends ConsumerStatefulWidget {
 
 class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
   List<dynamic> allOrders = [];
+  List<dynamic> filteredOrders = [];
   bool isLoading = true;
+  DateTime selectedDate = DateTime.now();
 
   @override
   void initState() {
     super.initState();
     _fetchUserRole();
   }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        selectedDate = pickedDate ?? DateTime.now();
+        print(selectedDate);
+        filterOrdersByDate(selectedDate!);
+      });
+    }
+  }
+
+  void filterOrdersByDate(DateTime date) {
+    final String formattedDate = DateFormat('yyyy-MM-dd').format(date);
+
+    setState(() {
+      filteredOrders = allOrders.where((order) {
+        final String fechaEntrega = DateFormat('yyyy-MM-dd')
+            .format(DateTime.parse(order['fecha_entrega']));
+        return fechaEntrega == formattedDate;
+      }).toList();
+    });
+  }
+
+
 
   Future<void> fetchOrders() async {
     try {
@@ -42,6 +75,7 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
         setState(() {
           allOrders = json.decode(response.body);
           isLoading = false;
+          filterOrdersByDate(selectedDate);
         });
       } else {
         throw Exception('Error al obtener los pedidos.');
@@ -61,7 +95,8 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
       final userId = await UserSession().getUserId();
 
       final response = await http.get(
-        Uri.parse('https://apiproyectomonarca.fly.dev/api/pedidos/obtener-por-repartidor/$userId'),
+        Uri.parse(
+            'https://apiproyectomonarca.fly.dev/api/pedidos/obtener-por-repartidor/$userId'),
         headers: {
           'Authorization': 'Bearer $token',
           'Content-Type': 'application/json',
@@ -84,7 +119,7 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
     }
   }
 
-   Future<void> _fetchUserRole() async {
+  Future<void> _fetchUserRole() async {
     try {
       final userId = await UserSession().getUserId();
 
@@ -99,7 +134,7 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
       }
     } catch (e) {
       setState(() {
-        isLoading = false; 
+        isLoading = false;
       });
       print(e);
     }
@@ -107,7 +142,6 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
 
   bool _hasValidRole(List<dynamic> roles) {
     for (var role in roles) {
-      
       if (role['id_rol'] != 2) {
         return true;
       }
@@ -137,20 +171,7 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
           const SnackBar(
               content: Text('Estado del pedido actualizado exitosamente')),
         );
-
-        if (newStatus == 'E') {
-          await handleDeliveredOrder(idPedido);
-        }
-        final userId = await UserSession().getUserId();
-
-      final response = await Dio().get(
-          'https://apiproyectomonarca.fly.dev/api/rolUsuarios/obtener-public/$userId');
-      final List<dynamic> roles = response.data;
-        if (_hasValidRole(roles)) {
         await fetchOrders();
-      } else {
-        await fetchOrdersByRepartidor();
-      }
       } else {
         throw Exception('Error al cambiar el estado del pedido.');
       }
@@ -163,54 +184,8 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
     }
   }
 
-  Future<void> handleDeliveredOrder(int idPedido) async {
-    try {
-      final keyValueStorageService = KeyValueStorageServiceImpl();
-      final token = await keyValueStorageService.getValue<String>('token');
-
-      final detailResponse = await http.get(
-        Uri.parse(
-            'https://apiproyectomonarca.fly.dev/api/balance/obtener-detalle-pedido/$idPedido'),
-        headers: {
-          'Authorization': 'Bearer $token',
-          'Content-Type': 'application/json',
-        },
-      );
-
-      if (detailResponse.statusCode != 200) {
-        throw Exception('Error al obtener el detalle del pedido.');
-      }
-
-      final pedidoDetalle = json.decode(detailResponse.body);
-
-      // Insertar el balance
-      final balanceResponse = await http.post(
-        Uri.parse('https://apiproyectomonarca.fly.dev/api/balance/insertar-balance'),
-        headers: {
-          'Authorization': 'Bearer $token',
-          'Content-Type': 'application/json',
-        },
-        body: json.encode(pedidoDetalle),
-      );
-
-      if (balanceResponse.statusCode != 200) {
-        throw Exception('Error al insertar el balance.');
-      }
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Balance insertado con éxito.')),
-      );
-    } catch (e) {
-      print('Error: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content: Text('Ocurrió un error al procesar el balance.')),
-      );
-    }
-  }
-
   Color getOrderCardColor(DateTime fechaEntrega, String estadoPedido) {
-    final now = DateTime.now().toUtc().add(const Duration(hours: -6));
+    final now = DateTime.now();
     final durationToDelivery = fechaEntrega.difference(now);
 
     if (estadoPedido == 'N') {
@@ -224,76 +199,11 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
     } else if (durationToDelivery.inMinutes > 0 &&
         durationToDelivery.inMinutes < 60) {
       return const Color(0xFFFFEB99);
-    } else if (now.isAfter(fechaEntrega) &&
-        now.difference(fechaEntrega).inMinutes >= 20) {
+    } else if (now.isAfter(fechaEntrega)) {
       return const Color(0xFFFF9999);
     } else {
       return Colors.blue;
     }
-  }
-
-  Future<void> showRouteDetails(int idPedido) async {
-    try {
-      final keyValueStorageService = KeyValueStorageServiceImpl();
-      final token = await keyValueStorageService.getValue<String>('token');
-
-      final response = await http.get(
-        Uri.parse(
-            'https://apiproyectomonarca.fly.dev/api/rutas/obtenerporPedido/$idPedido'),
-        headers: {
-          'Authorization': 'Bearer $token',
-          'Content-Type': 'application/json',
-        },
-      );
-
-      if (response.statusCode == 200) {
-        final routeDetails = json.decode(response.body);
-        _showRouteDialog(context, routeDetails['direccion']);
-      } else {
-        throw Exception('Error al obtener la ruta del pedido.');
-      }
-    } catch (e) {
-      print('Error: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Ocurrió un error al obtener la ruta.')),
-      );
-    }
-  }
-
-  void _showRouteDialog(BuildContext context, String direccion) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Detalles de la Ruta'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Dirección: $direccion'),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: direccion));
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text('Dirección copiada al portapapeles.')),
-                  );
-                },
-                child: const Text('Copiar dirección'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cerrar'),
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
@@ -301,14 +211,22 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de Pedidos'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.calendar_today),
+            onPressed: () {
+              _selectDate(context);
+            },
+          ),
+        ],
       ),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
               padding: const EdgeInsets.all(10),
-              itemCount: allOrders.length,
+              itemCount: filteredOrders.length,
               itemBuilder: (context, index) {
-                final order = allOrders[index];
+                final order = filteredOrders[index];
                 final DateTime fechaEntrega =
                     DateTime.parse(order['fecha_entrega']);
                 final String estadoPedido = order['estado_pedido'];
@@ -319,15 +237,13 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
                     color: const Color(0xFF99FF99),
                     alignment: Alignment.centerLeft,
                     padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child:
-                        const Icon(Icons.check, color: Colors.white, size: 40),
+                    child: const Icon(Icons.check, color: Colors.white, size: 40),
                   ),
                   secondaryBackground: Container(
                     color: const Color(0xFFFF6A6A),
                     alignment: Alignment.centerRight,
                     padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child:
-                        const Icon(Icons.cancel, color: Colors.white, size: 40),
+                    child: const Icon(Icons.cancel, color: Colors.white, size: 40),
                   ),
                   confirmDismiss: (direction) async {
                     if (direction == DismissDirection.endToStart) {
@@ -350,31 +266,13 @@ class _OrdersListScreenState extends ConsumerState<OrdersListScreen> {
                       subtitle: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                                                    Text(
-                            'Cliente: ${order['cliente']}',
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                          Text(
-                            'Repartidor: ${order['empleado']}',
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                          Text(
-                            'Fecha de Entrega: ${DateFormat('yyyy-MM-dd hh:mm a').format(fechaEntrega)}',
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                          Text(
-                            'Total: Q${order['totalpedido']}',
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                          Text(
-                            'Estado del Pedido: ${estadoPedido == 'A' ? 'Activo' : estadoPedido == 'E' ? 'Entregado' : 'Cancelado'}',
-                            style: const TextStyle(color: Colors.black),
-                          ),
+                          Text('Cliente: ${order['cliente']}'),
+                          Text('Repartidor: ${order['empleado']}'),
+                          Text('Fecha de Entrega: ${DateFormat('yyyy-MM-dd').format(fechaEntrega)}'),
+                          Text('Total: Q${order['totalpedido']}'),
+                          Text('Estado: ${estadoPedido == 'A' ? 'Activo' : estadoPedido == 'E' ? 'Entregado' : 'Cancelado'}'),
                         ],
                       ),
-                      onTap: () {
-                        showRouteDetails(order['id_pedido']);
-                      },
                     ),
                   ),
                 );
